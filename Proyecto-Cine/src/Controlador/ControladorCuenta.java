f0cf57d63bb460a7a1fa3f88bb359f88afe0cd62
Veronica Placencia <66141973+VeronicaPlacencia@users.noreply.github.com>
2021-02-04 05:19:31 +0100

    Creacion de controladores, y creacion de vistas
    Creacion de los metodos de los controladores 
    Creacion de la vista Mi Cuenta

---
+++ Proyecto-Cine/src/Controlador/ControladorCuenta.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author Victor Ayora, Geovanny Poma, Veronica Placencia, Azucena Toledo
+ */
+public class ControladorCuenta {
+    private  void  validacion () {
+
+    }
+}
---
+++ Proyecto-Cine/src/Controlador/ControladorRol.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author Veronica Estefania
+ */
+public class ControladorRol {
+    private  void  registrarUsuario () {
+
+    }
+    
+    public  boolean  validarCedula () {
+        
+        return false;
+    }
+    
+    private  void  darDeBajaUsuario () {
+
+    }
+    
+    private  void  modificarUsuario () {
+
+    }
+    
+    private  void  mostrarListaUsuario () {
+
+    }
+    
+    private  void  acceder () {
+
+    }
+}
